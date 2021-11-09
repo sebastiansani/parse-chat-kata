8d846292fbd5b4b5f0007d7fcd1ce96540f2c781
sebastiansani <n.sebastian.sani@gmail.com>
2021-11-09 22:21:11 +0100

    chat_parser works with multiple sentences

--- chat.py
+++ chat.py
@@ -19,11 +19,13 @@ def chat_parser(chat):
     if chat is None or chat == '':
         return []
     parsed_chat = []
-    sentence = {}
-    split_sentence = chat.split(' ')
-    sentence['date'] = split_sentence[0]
-    sentence['mention'] = split_sentence[0] + ' ' + split_sentence[1] + ' : '
-    sentence['sentence'] = ' '.join(split_sentence[3:])
-    sentence['type'] = split_sentence[1].lower()
-    parsed_chat.append(sentence)
+    for line in chat.splitlines(True):
+        sentence = {}
+        split_sentence = line.split(' ')
+        sentence['date'] = split_sentence[0]
+        sentence['mention'] = split_sentence[0] + ' ' + split_sentence[1] + ' : '
+        sentence['sentence'] = ' '.join(split_sentence[3:])
+        sentence['type'] = split_sentence[1].lower()
+        parsed_chat.append(sentence)
+
     return parsed_chat

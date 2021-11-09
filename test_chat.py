70932070ad89300010d17e26c8bf85e4ed217fa4
sebastiansani <n.sebastian.sani@gmail.com>
2021-11-09 22:11:18 +0100

    add multiple sentences test

--- test_chat.py
+++ test_chat.py
@@ -28,5 +28,21 @@ class TestChatParser(unittest.TestCase):
             "type": "customer"
         }])
 
+    def test_chat_parser_multiple_sentences(self):
+        chat = "14:24:32 Customer : Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n14:26:15 Agent : Aliquam non cursus erat, ut blandit lectus."
+        parsed_chat = chat_parser(chat)
+        self.assertEqual(parsed_chat, [{
+            "date": "14:24:32",
+            "mention": "14:24:32 Customer : ",
+            "sentence": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n",
+            "type": "customer"
+        }, {
+            "date": "14:26:15",
+            "mention": "14:26:15 Agent : ",
+            "sentence": "Aliquam non cursus erat, ut blandit lectus.",
+            "type": "agent"
+        }])
+
+
 if __name__ == "__main__":
     unittest.main()

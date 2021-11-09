ea111417dfcc08ac75f41356dc8b416a199ac53b
sebastiansani <n.sebastian.sani@gmail.com>
2021-11-09 22:48:08 +0100

    add date splitting test

--- test_chat.py
+++ test_chat.py
@@ -55,25 +55,42 @@ class TestChatParser(unittest.TestCase):
         )
         parsed_chat = chat_parser(chat)
         self.assertEqual(parsed_chat, [{
-            "date": '14:24:32',
-            "mention": '14:24:32 Customer : ',
-            "sentence": 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n',
-            "type": 'customer'
+            "date": "14:24:32",
+            "mention": "14:24:32 Customer : ",
+            "sentence": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n",
+            "type": "customer"
         }, {
-            "date": '14:27:00',
-            "mention": '14:27:00 Customer : ',
-            "sentence": 'Pellentesque cursus maximus felis, pharetra porta purus aliquet viverra.\n',
-            "type": 'customer'
+            "date": "14:27:00",
+            "mention": "14:27:00 Customer : ",
+            "sentence": "Pellentesque cursus maximus felis, pharetra porta purus aliquet viverra.\n",
+            "type": "customer"
         }, {
-            "date": '14:27:47',
-            "mention": '14:27:47 Agent : ',
-            "sentence": 'Vestibulum tempor diam eu leo molestie eleifend.\n',
-            "type": 'agent'
+            "date": "14:27:47",
+            "mention": "14:27:47 Agent : ",
+            "sentence": "Vestibulum tempor diam eu leo molestie eleifend.\n",
+            "type": "agent"
         }, {
-            "date": '14:28:28',
-            "mention": '14:28:28 Customer : ',
-            "sentence": 'Contrary to popular belief, Lorem Ipsum is not simply random text.',
-            "type": 'customer'
+            "date": "14:28:28",
+            "mention": "14:28:28 Customer : ",
+            "sentence": "Contrary to popular belief, Lorem Ipsum is not simply random text.",
+            "type": "customer"
+        }])
+
+    def test_chat_parser_date_splitting(self):
+        # Note: chat variable contains a single string, this format is used for readability.
+        chat = ("14:24:32 Customer : Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                "14:26:15 Agent : Aliquam non cursus erat, ut blandit lectus.")
+        parsed_chat = chat_parser(chat)
+        self.assertEqual(parsed_chat, [{
+            "date": "14:24:32",
+            "mention": "14:24:32 Customer : ",
+            "sentence": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            "type": "customer"
+        }, {
+            "date": "14:26:15",
+            "mention": "14:26:15 Agent : ",
+            "sentence": "Aliquam non cursus erat, ut blandit lectus.",
+            "type": "agent"
         }])
 
 
